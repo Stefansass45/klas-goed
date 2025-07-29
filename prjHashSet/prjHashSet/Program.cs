62c251c41c273f394a050a990ac486ce9d86b7c3
Stefansass45 <126760584+Stefansass45@users.noreply.github.com>
2025-07-29 09:53:46 +0200

    Add initial prjHashSet C# project
    Created a new C# project targeting .NET 8.0 with a Program.cs demonstrating basic HashSet operations. Includes solution and project files, build outputs, and Visual Studio configuration files.

---
+++ prjHashSet/prjHashSet/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace prjHashSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a HashSet to store unique superhero names
+            HashSet<string> superheroes = new HashSet<string>();
+
+            //add superhero names to the HashSet
+            superheroes.Add("Super?");
+            superheroes.Add("Wonder IT");
+            superheroes.Add("Batthey");
+            superheroes.Add("It Flash");
+            superheroes.Add("Aquait");
+
+            //attempt to add a duplicate superhero name
+            bool added = superheroes.Add("Super?");
+            Console.WriteLine($"Was Super? added again? {added}");
+
+            //Displa all superhero in the set
+            Console.WriteLine("\nList of Superheroes:");
+            foreach (string hero in superheroes)
+            {
+                Console.WriteLine(hero);
+            }
+
+            //Check if a specific superhero exists in the set
+            bool containsFlash = superheroes.Contains("The Flash");
+            Console.WriteLine($"\nIs It Flash in the set? {containsFlash}");
+
+            //Clear all superheroes from the set
+            superheroes.Clear();
+            Console.WriteLine("\n All Heroes have been removed");
+
+            //Check the count of superheroes in the set
+            Console.WriteLine($"Number of superheroes in the set: {superheroes.Count}");
+        }
+    }
+}
